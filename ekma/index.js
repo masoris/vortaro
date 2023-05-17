5afeb2169976bd5d0fe29edda5e94c853073f5f9
masoris <masoris@gmail.com>
2023-05-17 17:04:15 +0900

    0517

---
+++ ekma/index.js
@@ -0,0 +1,338 @@
+function E(e) { return document.getElementById(e); }
+
+function compress(original_string) {
+    // 문자열 압축
+    const encoder = new TextEncoder();
+    const data = encoder.encode(original_string);
+    const compressedData = pako.deflate(data);
+    // 압축된 데이터를 바이너리 형태로 출력
+    const binaryData = Array.from(compressedData, byte => String.fromCharCode(byte)).join('');
+    return binaryData;
+}
+
+function uncompress(binaryData) {
+    // 바이너리 데이터를 압축 해제하여 원래 문자열로 복원
+    const decodedData = binaryData.split('').map(char => char.charCodeAt(0));
+    const inflatedData = pako.inflate(new Uint8Array(decodedData));
+    const decoder = new TextDecoder();
+    const originalStringRestored = decoder.decode(inflatedData);
+    return originalStringRestored;
+}
+
+
+
+function call_ajax_text_get(url, parameters, callback) {
+    var req = false;
+    try {
+        req = new XMLHttpRequest();
+    } catch (e) {
+        try {
+            req = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                req = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {
+                req = false;
+            }
+        }
+    }
+
+    if (!req) { return; }
+
+    req.onreadystatechange = function () {
+        if (req.readyState == 4 && req.status == 200) {
+            callback(req.responseText);
+        }
+    };
+
+    req.open("GET", encodeURI(url), true);
+    req.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset='UTF-8'");
+    req.setRequestHeader('Ajax', 'true');
+
+    req.send(parameters);
+}
+
+function cxapelo(v) {
+    if (v.length > 100) { return v.subtring(0, 100); }
+    v = v.replace("cx", "ĉ").replace("gx", "ĝ").replace("hx", "ĥ");
+    v = v.replace("jx", "ĵ").replace("sx", "ŝ").replace("ux", "ŭ");
+    v = v.replace("Cx", "Ĉ").replace("Gx", "Ĝ").replace("Hx", "Ĥ");
+    v = v.replace("Jx", "Ĵ").replace("Sx", "Ŝ").replace("Ux", "Ŭ");
+    v = v.replace("ch", "ĉ").replace("gh", "ĝ").replace("hh", "ĥ");
+    v = v.replace("jh", "ĵ").replace("sh", "ŝ").replace("uh", "ŭ");
+    v = v.replace("Ch", "Ĉ").replace("Gh", "Ĝ").replace("Hh", "Ĥ");
+    v = v.replace("Jh", "Ĵ").replace("Sh", "Ŝ").replace("Uh", "Ŭ");
+    v = v.replace("c^", "ĉ").replace("g^", "ĝ").replace("h^", "ĥ");
+    v = v.replace("j^", "ĵ").replace("s^", "ŝ").replace("u^", "ŭ");
+    v = v.replace("C^", "Ĉ").replace("G^", "Ĝ").replace("H^", "Ĥ");
+    v = v.replace("J^", "Ĵ").replace("S^", "Ŝ").replace("U^", "Ŭ");
+    v = v.replace("au", "aŭ").replace("eu", "eŭ").replace("Au", "Aŭ").replace("Eu", "Eŭ");
+    v = v.replace("ŭx", "ŭ").replace("Ŭx", "Ŭ").replace("w", "ŭ").replace("W", "Ŭ");
+    return v;
+}
+
+var DIC = null;
+var first = 1;
+
+function klaku_vorto(v) {
+    if (v == "") return;
+
+    if (DIC == null) {
+        alert("Vi ankoraŭ ne elŝutis datumon.\r\n아직 사전 데이타를 내려 받지 않았습니다. 내려받기를 누르세요.");
+        return;
+    }
+
+    E("rezulto").innerHTML = "<br>Searching...";
+
+    var result_lines = search(DIC, v);
+    if (result_lines.length == 0) {
+        v = v.toLowerCase();
+        result_lines = search(DIC, v);
+        if (result_lines.length == 0) {
+            E("rezulto").innerHTML = "<br><br>Nenio troviĝas por tio.<br>단어를 찾을 수 없습니다.";
+            return;
+        }
+    }
+
+    var result = highlight(result_lines, v);
+
+    var str = "";
+    for (var i = 0; i < result.length; i++) {
+        str += result[i] + "\n";
+    }
+
+    E("rezulto").innerHTML = str;
+
+    first = 1;
+}
+
+function has_cxapelon(v) {
+    var X = "xX^h";
+    for (var i = 0; i < v.length; i++) {
+        if (X.indexOf(v.charAt(i)) >= 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function chapeligo() {
+    var v1 = E("vorto").value;
+    if (has_cxapelon(v1)) {
+        // alert(v1);
+        E("vorto").value = '';
+        var v2 = cxapelo(v1);
+        // alert(v2);
+        E("vorto").style.display = '';
+        E("vorto").value = v2;
+    }
+}
+
+function highlight(lines, vorto) {
+    var new_lines = new Array();
+
+    var suffix = vorto.substring(vorto.length - 1, vorto.length);
+    if (vorto.length > 2 && "aeiou-/".indexOf(suffix) >= 0) {
+        vorto = vorto.substring(0, vorto.length - 1);
+    }
+
+    for (var i = 0; i < lines.length; i++) {
+        var x = new_lines.length;
+        new_lines[x] = lines[i];
+        new_lines[x] = new_lines[x].split(vorto).join("<font color=blue><b>" + vorto + "</b></font>");
+        new_lines[x] = "<b>" + new_lines[x].replace("\t", "</b>&nbsp;&nbsp;") + "<hr>";
+        if (new_lines[x].indexOf("<G_1>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_1>", "<B><font color=green size=-1>[G1]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_2>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_2>", "<B><font color=green size=-1>[G2]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_3>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_3>", "<B><font color=green size=-1>[G3]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_4>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_4>", "<B><font color=green size=-1>[G4]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_5>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_5>", "<B><font color=green size=-1>[G5]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_6>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_6>", "<B><font color=green size=-1>[G6]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_7>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_7>", "<B><font color=green size=-1>[G7]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_8>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_8>", "<B><font color=green size=-1>[G8]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_9>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_9>", "<B><font color=green size=-1>[G9]</font></B>");
+        }
+        if (new_lines[x].indexOf("<O_A>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<O_A>", "<B><font color=green size=-1>[OA]</font></B>");
+        }
+        if (new_lines[x].indexOf("<G_Y>") >= 0) {
+            new_lines[x] = new_lines[x].replace("<G_Y>", "");
+        }
+    }
+
+    return new_lines;
+}
+
+function is_separator(c) {
+    if (c == ' ' || c == ',' || c == '.' || c == '[' ||
+        c == ']' || c == ')' || c == '(' || c == '<' ||
+        c == '>' || c == '-' || c == '=') {
+        return 1;
+    }
+
+    return 0;
+}
+
+function score(word, entry) {
+    var score = 0;
+
+    var suffix = word.substring(word.length - 1, word.length);
+    if (word.length > 2 && "aeiou-/".indexOf(suffix) >= 0) {
+        word = word.substring(0, word.length - 1);
+    }
+
+    var idx = entry.indexOf(word);
+    if (idx == 0) {
+        score += 30;
+    }
+
+    if (idx > 0 && is_separator(entry.charAt(idx - 1))) {
+        score += 20;
+    }
+
+    if (idx + word.length + 1 < entry.length) {
+        if (is_separator(entry.charAt(idx + word.length))) {
+            if (idx == 0) {
+                score += 50;
+            } else {
+                score += 3;
+            }
+        }
+    }
+
+    var splitted = entry.split(word);
+    score += splitted.length;
+
+    score = score + 10000;
+
+    return "{" + score + "}" + entry;
+}
+
+function search(dic, word) {
+    var result = new Array();
+    var found = new Object;
+
+    if (word.length < 2) {
+        return result;
+    }
+
+    for (var i = 0; i < dic.length; i++) {
+        if (word.length == 1) {
+            if (dic[i].indexOf(word) == 0) {
+                result[result.length] = score(word, dic[i]);
+                found[i] = 1;
+            }
+        } else {
+            if (dic[i].indexOf(word) >= 0) {
+                result[result.length] = score(word, dic[i]);
+                found[i] = 1;
+            }
+        }
+    }
+
+    var suffix = word.substring(word.length - 1, word.length);
+    if (word.length > 2 && "aeiou-/".indexOf(suffix) >= 0) {
+        var word2 = word.substring(0, word.length - 1);
+        for (var i = 0; i < dic.length; i++) {
+            if (!(i in found)) {
+                if (dic[i].indexOf(word2) >= 0) {
+                    result[result.length] = score(word, dic[i]);
+                }
+            }
+        }
+    }
+
+    result.sort();
+    result.reverse();
+
+    for (var i = 0; i < result.length; i++) {
+        result[i] = result[i].substring(7);
+    }
+
+    return result;
+}
+
+var load_status = "INIT";
+
+window.onload = function () {
+    E("vorto").onkeyup = function (e) {
+        chapeligo();
+        if (e.key == "Enter") {
+            klaku_vorto(E("vorto").value);
+        }
+    }
+
+    E("vorto").onclick = function () {
+        if (first) {
+            E("vorto").select();
+            first = 0;
+        }
+    }
+
+    E("Serchu").onclick = function () {
+        chapeligo();
+        klaku_vorto(E("vorto").value);
+        first = 1;
+    }
+
+    if (localStorage['DIC_JSON_EKMA'] != null) {
+        DIC = uncompress(localStorage.DIC_JSON_EKMA).split('\n');
+    }
+
+    if (DIC != null) {
+        E("load").value = "Datumo preta. 사전검색 가능함.";
+        load_status = "DONE";
+    }
+
+    E("Reset").onclick = function () {
+        delete localStorage.DIC_JSON_EKMA;
+        DIC = null;
+        load_status = "INIT";
+        E("load").value = "Elŝutu datumon. 사전 내려받기.";
+        E("Reset").disabled = true;
+    };
+
+    E("load").onclick = function () {
+        if (load_status == "PROGRESS") {
+            alert("Elŝutanta ... 다운로드 진행중.");
+            return;
+        }
+
+        // alert("내려받기에 시간이 걸릴 수 있으므로, OK/예 버튼을 누른 후, 잠시 기다려 주시기 바랍니다.\r\n내려 받는 동안 인터넷이 연결되어 있어야 합니다.");
+        DIC = null;
+        delete localStorage.DIC_JSON_EKMA;
+        E("load").value = "Elŝutanta ... 다운로드 진행중.";
+        load_status = "PROGRESS";
+        call_ajax_text_get("./dic.tsv", "time=" + new Date(),
+            function (resp) {
+                // localStorage.clear();
+                compressed = compress(resp);
+                // alert(compressed.length);
+                // alert(resp.length);
+                localStorage['DIC_JSON_EKMA'] = compressed;
+
+                DIC = resp.split('\n');
+                E("load").value = "Datumo preta. 사전검색 가능함.";
+                load_status = "DONE";
+                E("Reset").disabled = false;
+
+            }
+        );
+    };
+};
--- vortaro.py
+++ vortaro.py
@@ -9,4 +9,22 @@ def serve_ex(path):
     # response.headers['Cache-Control'] = 'max-age=3600'
     return response
 
+@app.route('/th/<path:path>')
+def serve_th(path):
+    response = make_response(send_from_directory('./th', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+@app.route('/ch/<path:path>')
+def serve_ch(path):
+    response = make_response(send_from_directory('./ch', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+@app.route('/ekma/<path:path>')
+def serve_ekma(path):
+    response = make_response(send_from_directory('./ekma', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
 app.run(debug=True, host='192.168.117.129', port=5005)
