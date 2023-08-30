359e115ec82888c8e2a30b0de345c53aeae0eb01
masoris <masoris@gmail.com>
2023-08-30 16:58:20 +0900

    0830

--- vortaro.py
+++ vortaro.py
@@ -1,7 +1,68 @@
-from flask import Flask, send_from_directory, request, make_response, jsonify, url_for
+from flask import Flask, send_from_directory, request, make_response, jsonify, url_for, redirect
 import subprocess, os, sys, shutil
 
 app = Flask(__name__)
+
+@app.route('/index.html')
+def serve_index():
+    return redirect('/ekma/index.html')
+
+@app.route('/')
+def serve_root():
+    return redirect('/ekma/index.html')
+ #   response = make_response(send_from_directory('./ex', 'index.html'))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+  #  return response
+
+@app.route('/ex', defaults={'path': 'index.html'})
+@app.route('/ex/', defaults={'path': 'index.html'})
+@app.route('/ex/<path:path>')
+def serve_ex(path):
+#    if path == '' or path == None: path = 'index.html'
+    response = make_response(send_from_directory('./ex', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+@app.route('/th', defaults={'path': 'index.html'})
+@app.route('/th/', defaults={'path': 'index.html'})
+@app.route('/th/<path:path>')
+def serve_th(path):
+#    if path == '' or path == None: path = 'index.html'
+    response = make_response(send_from_directory('./th', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+@app.route('/tw', defaults={'path': 'index.html'})
+@app.route('/tw/', defaults={'path': 'index.html'})
+@app.route('/tw/<path:path>')
+def serve_tw(path):
+#    if path == '' or path == None: path = 'index.html'
+    response = make_response(send_from_directory('./tw', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+
+@app.route('/b', defaults={'path': 'index.html'})
+@app.route('/b/', defaults={'path': 'index.html'})
+@app.route('/b/<path:path>')
+def serve_b(path):
+#    if path == '' or path == None: path = 'index.html'
+    response = make_response(send_from_directory('./b', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
+
+@app.route('/ekma', defaults={'path': 'index.html'})
+@app.route('/ekma/', defaults={'path': 'index.html'})
+@app.route('/ekma/<path:path>')
+def serve_ekma(path):
+    print(path)
+#    if path == '' or path == None or path == '/': path = 'index.html'
+#    elif path[0] == '/': path = path[1:]
+    response = make_response(send_from_directory('./ekma', path))
+    # response.headers['Cache-Control'] = 'max-age=3600'
+    return response
+
 
 # favicon.ico 파일 서비스
 @app.route('/favicon.ico')
